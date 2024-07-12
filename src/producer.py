459e2317e712d058b53e9b6d97c0c22124bbb66e
RokoSi <yurf3z0@gmail.com>
2024-07-12 12:13:35 +0300

    поправил settings

--- src/producer.py
+++ src/producer.py
@@ -1,17 +1,22 @@
 import json
 from confluent_kafka import Producer
 
+from src.settings import settings
+
 
 def get_msg_json(user_json: list):
-
+    """
+    Отправляет сообщения через kafka
+    :param user_json: полученный пользователь
+    """
     producer = Producer({'bootstrap.servers': 'harmless-llama-10955-eu2-kafka.upstash.io:9092',
                          'sasl.mechanism': 'SCRAM-SHA-256',
                          'security.protocol': 'SASL_SSL',
-                         'sasl.username': 'aGFybWxlc3MtbGxhbWEtMTA5NTUkSWlsftAT5bb2G5AxTAXsG48EcNi8Pk20sDU',
-                         'sasl.password': 'YzI5N2JhYzQtZGJjMi00YjJmLThjOTQtMTEwNzhiZjQ3MmNm'
+                         'sasl.username': settings.user_kafka,
+                         'sasl.password': settings.password_kafka
                          })
     data_json = json.dumps(user_json)
 
-    producer.produce('test_msg', value=data_json)
+    producer.produce(settings.topic, value=data_json)
     producer.flush()
     print("Message sent successfully")
--- src/settings.py
+++ src/settings.py
@@ -1,24 +1,15 @@
-import os
-from typing import Optional, ClassVar
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=[
-            os.path.join(os.path.join(os.path.dirname(os.getcwd()), ".env.dev")),
-            os.path.join(os.path.join(os.path.dirname(os.getcwd()), ".env.secret")),
-        ],
+        env_file=("../.env.dev", "../src/.env.secret"),
         env_file_encoding="utf-8",
     )
-    host: Optional[str] = os.getenv("HOST")
-    user: Optional[str] = os.getenv("USER")
-    password: Optional[str] = os.getenv("PASSWORD")
-    db: Optional[str] = os.getenv("DB")
-    port_env: ClassVar[Optional[str]] = os.getenv("PORT")
-    port: Optional[int] = int(port_env) if port_env is not None else None
-    url: Optional[str] = os.getenv("URL")
+    url: str
+    user_kafka: str
+    password_kafka: str
+    topic: str
 
 
 settings = Settings()
